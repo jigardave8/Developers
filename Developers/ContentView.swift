20b3b55a94c466aebd2396ffbf2b970c6de1d29c
Jigar Dave <jdavenz8@gmail.com>
2021-06-18 10:16:37 +0530

    Initial Commit

---
+++ Developers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Developers
+//
+//  Created by jigar dave on 18/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Developers/DevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopersApp.swift
+//  Developers
+//
+//  Created by jigar dave on 18/06/21.
+//
+
+import SwiftUI
+
+@main
+struct DevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
